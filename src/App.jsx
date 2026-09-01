587aa2961ebe2360b7e43beb9b1f5dcd1cc8fd4e
Jorge E. Ledesma Cruz <jorge.ledesma6417@alumnos.udg.mx>
2026-09-01 03:47:59 -0600

    chore: preparar estructura inicial del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <header>
+        <h1>Sistema Web de Gestión de Citas</h1>
+
+        <p>
+          Proyecto de reservación de citas para barbería y salones de belleza. Desarrollado con React. 
+        </p>
+      </header>
+    </main>
+  )
+}
+
+export default App
